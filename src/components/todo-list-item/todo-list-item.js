689ca204096138e6709b0ec3ff412b2d7b2b754d
Danil Shchepkov <daniil.s.h.e.p.k.o.v@gmail.com>
2020-05-01 15:56:33 +0300

    изменение статусов todoListItem происходит в обе стороны

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -6,42 +6,54 @@ import './todo-list-item.css';
 
 
  export default class TodoListItem extends Component{
-     state = {
-         done: false
-     };
 
+     state = {
+         done: false,
+         important:false
+
+     };
 
      onLabelClick = () => {
-         this.setState({done: true})
+         this.setState((state) => {
+             return {
+                 done: !state.done
+             }
+         })
+     };
+     onMarckImportant = () => {
+         this.setState((state) => {
+            return {
+                important: !state.important
+            }
+         });
      };
 
+
     render() {
-        const { label, important = false } = this.props;
-        const { done } = this.state;
+        const { label } = this.props;
+        const { done, important } = this.state;
 
         let classNames = 'todo-list-item';
         if (done) {
             classNames += " done"
         }
 
+        if (important) {
+            classNames += ' important';
 
-
-        const style = {
-            color: important ? 'steelblue' : 'black',
-            fontWeight: important ? 'bold' : 'normal'
-        };
+        }
 
         return (
             <span className={ classNames }>
               <span
                   className="todo-list-item-label"
-                  style={style}
-                    onClick={ this.onLabelClick }>
+                  onClick={ this.onLabelClick }>
                 {label}
               </span>
 
               <button type="button"
-                      className="btn btn-outline-success btn-sm float-right">
+                      className="btn btn-outline-success btn-sm float-right"
+                        onClick={ this.onMarckImportant }>
                 <i className="fa fa-exclamation" />
               </button>
 
