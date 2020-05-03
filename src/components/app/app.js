53514ab4397aff482558eedc921758fa3908e661
Danil Shchepkov <daniil.s.h.e.p.k.o.v@gmail.com>
2020-05-03 14:01:42 +0300

    Добавлен по элементам

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -18,7 +18,8 @@ export default class App extends Component{
             this.createTodoItem('Drink Coffee'),
             this.createTodoItem('Make Awesome App'),
             this.createTodoItem('Have a lunch')
-        ]
+        ],
+        term: ''
     };
 
 
@@ -86,8 +87,24 @@ export default class App extends Component{
         ];
     }
 
+    search(items, term) {
+        if (term.length === 0) {
+            return items;
+        }
+        
+        return items.filter((item) => {
+            return item.label
+                .toLowerCase().indexOf(term.toLowerCase()) > -1;
+        });
+    };
+
+    onSearchChange = (term) => {
+        this.setState({term})
+    };
+
     render(){
-        const { todoData } = this.state;
+        const { todoData, term } = this.state;
+        const visibleItem = this.search(todoData, term);
         const doneCount = todoData
             .filter((el) => el.done).length;
         const todoCount = todoData.length - doneCount;
@@ -96,11 +113,12 @@ export default class App extends Component{
             <div className="todo-app">
                 <AppHeader toDo={todoCount} done={doneCount}/>
                 <div className="top-panel d-flex">
-                    <SearchPanel/>
+                    <SearchPanel
+                        onSearchChange = {this.onSearchChange}/>
                     <ItemStatusFilter/>
                 </div>
 
-                <TodoList todos={todoData}
+                <TodoList todos={visibleItem}
                     onDeleted = { this.deleteItem }
                     onToggleImportant = { this.onToggleImportant }
                     onToggleDone = { this.onToggleDone }
