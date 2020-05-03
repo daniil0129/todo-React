26582252326366030c6fc46a6f0f89d27ee3bdeb
Danil Shchepkov <daniil.s.h.e.p.k.o.v@gmail.com>
2020-05-03 16:05:48 +0300

    Добавле функциона для кнопак button-status-filter

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -19,7 +19,8 @@ export default class App extends Component{
             this.createTodoItem('Make Awesome App'),
             this.createTodoItem('Have a lunch')
         ],
-        term: ''
+        term: '',
+        filter: 'active'
     };
 
 
@@ -102,9 +103,27 @@ export default class App extends Component{
         this.setState({term})
     };
 
+    filter(items, filter) {
+        switch (filter) {
+            case 'all':
+                return items;
+            case 'active':
+                return items.filter((item) => !item.done);
+            case 'done':
+                return items.filter((item) => item.done);
+            default:
+                return items;
+        }
+    };
+
+    onFilterChange = (filter) => {
+      this.setState({filter});
+    };
+
     render(){
-        const { todoData, term } = this.state;
-        const visibleItem = this.search(todoData, term);
+        const { todoData, term, filter } = this.state;
+        const visibleItem = this.filter
+            (this.search(todoData, term), filter);
         const doneCount = todoData
             .filter((el) => el.done).length;
         const todoCount = todoData.length - doneCount;
@@ -115,7 +134,9 @@ export default class App extends Component{
                 <div className="top-panel d-flex">
                     <SearchPanel
                         onSearchChange = {this.onSearchChange}/>
-                    <ItemStatusFilter/>
+                    <ItemStatusFilter
+                        filter = {filter}
+                        onFilterChange = {this.onFilterChange}/>
                 </div>
 
                 <TodoList todos={visibleItem}
